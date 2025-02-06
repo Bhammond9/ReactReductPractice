9cd59819489f2f96e5aba8d90b82a64b1f88b65b
Bhammond9 <bhammond901@yahoo.com>
2025-02-05 17:39:39 -0700

    Added useGetPostsQueryHook to PostsList.tsx

--- src/features/posts/PostsList.tsx
+++ src/features/posts/PostsList.tsx
@@ -3,12 +3,21 @@ import { Link } from 'react-router-dom'
 import { useAppSelector } from '@/app/hooks'
 import { TimeAgo } from '@/components/TimeAgo'
 
+import { useGetPostsQuery, Post } from '@/features/api/apiSlice'
+
 import { PostAuthor } from './PostAuthor'
 import { selectAllPosts } from './postsSlice'
 import { ReactionButtons } from './ReactionButtons'
 
 export const PostsList = () => {
   const posts = useAppSelector(selectAllPosts)
+
+// Go back to passing a `post` object as a prop
+interface PostExcerptProps {
+  post: Post
+}
+
+function PostExcerpt({ post }: PostExcerptProps) {
 
   // Sort posts in reverse chronological order by datetime string
   const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
@@ -27,6 +36,28 @@ export const PostsList = () => {
     </article>
   ))
 
+
+export const PostsList = () => {
+  // Calling the `useGetPostsQuery()` hook automatically fetches data!
+  const {
+    data: posts = [],
+    isLoading,
+    isSuccess,
+    isError,
+    error
+  } = useGetPostsQuery()
+
+  let content: React.ReactNode
+
+  // Show loading states based on the hook status flags
+  if (isLoading) {
+    content = <Spinner text="Loading..." />
+  } else if (isSuccess) {
+    content = posts.map(post => <PostExcerpt key={post.id} post={post} />)
+  } else if (isError) {
+    content = <div>{error.toString()}</div>
+  }
+
   return (
     <section className="posts-list">
       <h2>Posts</h2>
@@ -34,3 +65,5 @@ export const PostsList = () => {
     </section>
   )
 }
+
+
